90065747544bc20e9e5973b6532472b939941809
theChosenDevop <tobbey.adesanya@gmail.com>
2024-01-20 07:38:00 -0800

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # Using Puppet, install flask from pip3.
 package { 'python3-pip':
-  ensure => installed,
+  ensure    => installed,
 }
 
 package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip',
-  require   => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
