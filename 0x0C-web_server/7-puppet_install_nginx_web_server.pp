494f820da2898a8d4523d74f6b2dfff6a1a5a707
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-01 21:30:59 -0800

    update task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+--Configure nginx using puppet
+
+package { 'nginx':
+  ensure => 'installed'
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://github.com/theChosenDevop permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
